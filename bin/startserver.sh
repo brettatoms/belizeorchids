78fffd125f78d9c2acdbdd3623c8c6f60ee867c2
Brett <brettatoms@gmail.com>
2013-09-20 13:47:03 -0400

    add our server start script from when we were playing with mongodb

---
+++ bin/startserver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#curl --data 'name=master&server=prod.master.example.com' http://localhost:27080/_connect
+BASEPATH=`dirname $0`
+MONGOOSE_PATH=$BASEPATH/sleepy.mongoose
+
+if ! [ -d $MONGOOSE_PATH ] ; then
+    git clone git@github.com:10gen-labs/sleepy.mongoose.git $BASEPATH
+fi
+
+if [ "`pidof mongod`" == "" ] ; then
+    echo "starting mongod..."
+    mongod &
+    sleep 1
+else
+    echo "mongod already running."
+fi
+
+python $MONGOOSE_PATH/httpd.py -x -m "localhost:27017" &
+# sleep 5
+# curl  --data "server=localhost:27017" http://localhost:27080/_connect 
+
+
+
