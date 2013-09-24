b5ae5b31d05cebce27ee52d8cfc4f90b4295f347
Brett <brettatoms@gmail.com>
2013-09-24 11:51:07 -0400

    sex it up a little

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,7 +164,7 @@ module.exports = function (grunt) {
                 imagesDir: '<%= yeoman.app %>/images',
                 javascriptsDir: '<%= yeoman.app %>/scripts',
                 fontsDir: '<%= yeoman.app %>/styles/fonts',
-                importPath: '<%= yeoman.app %>/components',
+                importPath: '<%= yeoman.app %>/bower_components',
                 httpImagesPath: '/images',
                 httpGeneratedImagesPath: '/images/generated',
                 httpFontsPath: '/styles/fonts',
