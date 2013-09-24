cd8ba03581119887b6f1efc0e4d808931d6f1805
--set <brettatoms@gmail.com>
2013-09-23 22:07:28 -0400

    find the orchid counts when loading

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,19 +3,46 @@
 angular.module('belizeorchidsApp')
     .controller('MainCtrl', function ($http, $scope) {
         $scope.orchids = [];
+        $scope.orchid_counts = {}
+        var db = 'http://localhost:27080/orchids';
 
         var config = {
             method: 'GET',
-            url: "http://localhost:27080/orchids/orchids/_find",
+            url: db + "/orchids/_find",
             params: {
                 limit: "500",
                 batch_size: "500"
             }
         }
+
+        $scope.get_counts = function(name) {
+            console.log("name: ", name);
+            console.log('{"name": ' + name + '}')
+            var config = {
+                method: 'GET',
+                url: db + "/thumbs/_find",
+                params: {
+                    criteria: JSON.stringify({"name": name})
+                }
+            };
+            $http(config)
+                .success(function(data, status, headers, config) {
+                    console.log("config: ", config);
+                    console.log("data: ", data);
+                    $scope.orchid_counts[name] = data.results.length
+                })
+                .error(function(data, status, headers, config) {
+                    // do something
+                })
+        }
+
         $http(config)
             .success(function(data, status, headers, config) {
                 $scope.orchids = data.results;
-                console.log("data: ", data);
+                data.results.forEach(function(orchid) {
+                    $scope.get_counts(orchid.name);
+                });
+                //console.log("data: ", data);
             })
             .error(function(data, status, headers, config) {
                 // do something
